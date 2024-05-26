2977cff1e124f6a57b7bfa971b533deef9912303
imajo <30540418+imajoriri@users.noreply.github.com>
2024-05-27 08:24:40 +0900

    Update cmagic.rb

--- cmagic.rb
+++ cmagic.rb
@@ -2,7 +2,7 @@ class Cmagic < Formula
   desc ""
   homepage "https://github.com/imajoriri/codemagic-builder"
   url "https://github.com/imajoriri/codemagic-builder/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "1c8c0761a79723b974142608f92c4d460d5758ab23d0220d6a4a48541d7af162"
+  sha256 "a4fe4c99081fe75ec6f5c3c9d51225830d5b424fa01b713dfde1e2cd5070620f"
   version "0.0.2"
   license ""
 
