a73ea8fa157581310456993afc26f8807f767db7
imajo <30540418+imajoriri@users.noreply.github.com>
2024-05-26 15:18:06 +0900

    Create cmagic.rb

---
+++ cmagic.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Cmagic < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/imajoriri/codemagic-builder/archive/refs/tags/0.0.1.tar.gz"
+  sha256 "169af048dc476dcd7345310d55a86c36a63b5c17f64d1b46f06de85ad8ecdecc"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    # system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "codemagic_builder"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test cmagic`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
