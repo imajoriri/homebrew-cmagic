054cecd18e71569596b5a5ff13404a3e2049b466
imajo takeyuki <30540418+imajoriri@users.noreply.github.com>
2026-02-20 10:31:14 +0900

    Bump Cmagic version to 0.0.6
    Updated the URL and version for the Cmagic formula.

--- cmagic.rb
+++ cmagic.rb
@@ -1,7 +1,7 @@
 class Cmagic < Formula
   desc ""
   homepage "https://github.com/imajoriri/codemagic-builder"
-  url "https://github.com/imajoriri/codemagic-builder/archive/refs/tags/v0.0.5.tar.gz"
+  url "https://github.com/imajoriri/codemagic-builder/archive/refs/tags/v0.0.6.tar.gz"
   sha256 "4bfdc2ab23489798eec6cc07b3e580cc3758eb023d23dcb743bdf1c390f4c5f9"
   version "0.0.5"
   license ""
