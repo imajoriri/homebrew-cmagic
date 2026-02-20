6429f3c0f8155482ca33da1959f58145b815cada
imajo takeyuki <30540418+imajoriri@users.noreply.github.com>
2026-02-20 10:34:37 +0900

    Update version and SHA256 for Cmagic formula

--- cmagic.rb
+++ cmagic.rb
@@ -2,8 +2,8 @@ class Cmagic < Formula
   desc ""
   homepage "https://github.com/imajoriri/codemagic-builder"
   url "https://github.com/imajoriri/codemagic-builder/archive/refs/tags/v0.0.6.tar.gz"
-  sha256 "4bfdc2ab23489798eec6cc07b3e580cc3758eb023d23dcb743bdf1c390f4c5f9"
-  version "0.0.5"
+  sha256 "cca73e5c78a1918d6ad7b5412d75de27a4e05e6c9b195a3c6682f391f46afc3d"
+  version "0.0.6"
   license ""
 
   def install
